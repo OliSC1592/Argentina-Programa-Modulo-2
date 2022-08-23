68063c3e5258a326a3c656a0f03d48bc64b37906
Olivia S.C <110567852+OliSC1592@users.noreply.github.com>
2022-08-23 17:20:46 -0300

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//     $ (".collapse.show").each(function() { 
+//         $(this).prev(".card-header").find("fa").addClass("fa-minus").removeClass("fa-plus");
+//         console.log("Entro en colapse show");
+//     });
+
+//     $(".collapse").on('show.bs.collapse',function(){
+//         $(this).prev(".card-header").find(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
+//         console.log("Entro en remove plus");
+//     }).on('hide.bs.collapse',function(){
+//         $(this).prev(".card-header").find(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
+//         console.log("Entro en remove minus");
+//     });
+    
+// });
